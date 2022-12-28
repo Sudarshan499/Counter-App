cb75ffa122cba79c2a825d88f8170c598d35231f
Sudarshan499 <121094581+Sudarshan499@users.noreply.github.com>
2022-12-28 19:52:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React,{ useState } from "react";
+const App =() => {
+
+  const [count, setCount] = useState(0);
+  const increament = () => setCount (count + 1);
+  const decreament = () => setCount (count - 1);
+
+
+  return(
+  <center>
+    <div>
+      <h1>Counter App</h1>
+      <h2>Total {count}</h2>
+
+      <button onClick={increament}>increament</button>
+      <button onClick={decreament}>decreament</button>
+    </div>
+  </center>
+  )
+}
+export default App;
